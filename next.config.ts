8bd56469778604cb48d245b55a754976614d0e30
Hmanprod <hmanprod@gmail.com>
2025-06-08 22:04:42 +0300

    chore: remove debug logs from Dockerfile and update Cloudinary image config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,8 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
+        // Ajout/modification de cette ligne :
+        pathname: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME ? `/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/**` : '/**',
       }
     ],
     formats: ['image/avif', 'image/webp'],
